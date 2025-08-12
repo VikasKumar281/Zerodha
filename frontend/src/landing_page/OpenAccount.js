83da3a33ed8d5d58545ddfe4499b23cc8812b8e4
VikasKumar281 <vikaskumar280204@gmail.com>
2025-08-12 23:41:57 +0530

    `feat: updated index.css and OpenAccount.js files with new styles and content`

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../index.css"; 
 
 function OpenAccount() {
   return (
@@ -10,7 +11,7 @@ function OpenAccount() {
           F&O trades.
         </p>
         <button
-          className="p-2 btn btn-primary fs-5 mb-5"
+          className="signup-btn p-2 btn fs-5 mb-5"
           style={{ width: "20%", margin: "0 auto" }}
         >
           Sign up Now
